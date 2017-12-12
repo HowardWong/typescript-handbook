285fd000417b2f7ac54d216d49ad3b249af46ec9
王旭浩 <AlohaWangxh@163.com>
2017-12-12 23:22:08 +0800

    update

---
+++ basic/classes.ts
@@ -0,0 +1,31 @@
+// declare a class
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("world");
+
+
+// class inherite
+class Animal {
+  move(distanceInMeters: number = 0) {
+    console.log(`Animal moved ${distanceInMeters}m.`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log('Woof! Woof!');
+  }
+}
+
+const dog = new Dog();
+dog.bark();
+dog.move(10);
+dog.bark();
